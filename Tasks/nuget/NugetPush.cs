fe5016b078ecfeee97b68f50ae40a1903789aa18
Amedeo Gautiero <amedeo.gautiero@lynxspa.com>
2024-10-25 00:48:46 +0200

    init

---
+++ Tasks/nuget/NugetPush.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace damgau.msbuild.tasks.nuget
+{
+    public class NugetPush : Task
+    {
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+}
